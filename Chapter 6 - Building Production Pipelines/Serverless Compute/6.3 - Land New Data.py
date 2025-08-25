188ed13ddd3ea79390a706f515ccd136886f438a
nachobot <nachobot>
2025-08-25 19:29:44 +0000

    Updates for Serverless Setup and DLT Pipeline configuration

---
+++ Chapter 6 - Building Production Pipelines/Serverless Compute/6.3 - Land New Data.py
@@ -0,0 +1,16 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Processing Change Data Capture
+
+# COMMAND ----------
+
+# MAGIC %run ../../Includes/Serverless-School-Setup
+
+# COMMAND ----------
+
+load_new_json_data()
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * from json.`dbfs:/mnt/DE-Associate-Book/datasets/school/courses-cdc/02.json`
